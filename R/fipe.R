fe5bae0ff4d34d464425deb972609bae5ef00678
italocegatta <italocegatta@gmail.com>
2019-08-21 21:40:26 -0300

    typo

--- R/fipe.R
+++ R/fipe.R
@@ -1,13 +1,13 @@
 #' Access to the Fipe Database
 #'
-#' @param model A character vector. Vehicle model name.
-#' @param make A character vector. If NULL, search all models in all makes, otherwise only those indicated.
-#' @param year A numeric vector. Year of manufacture of the vehicle. If 0 returns vehicles 0 km.
-#' @param date A date vector. Reference date for the vehicle price.
-#' @param progress A logical, if TRUE print a progress bar.
-#' @param parallel A logical, if TRUE apply function in parallel.
+#' @param model a character vector. Vehicle model name.
+#' @param make a character vector. If NULL, search all models in all makes, otherwise only those indicated.
+#' @param year a numeric vector. Year of manufacture of the vehicle. If 0 returns vehicles 0 km.
+#' @param date a date vector. Reference date for the vehicle price.
+#' @param progress a logical, if TRUE print a progress bar.
+#' @param parallel a logical, if TRUE apply function in parallel.
 #'
-#' @return A data frame including model, make, year, date and price.
+#' @return A data frame/tibble including model, make, year, date and price.
 #'
 #' @details The Fipe Database shows average prices of vehicles on the national
 #'   market, functioning only as a parameter for negotiations and evaluations.
@@ -19,7 +19,7 @@
 #'   expressed in R$ (reais) of the month/year of reference.
 #'
 #' @examples
-#' fipe_vehicle(model = "etios", make = "toyota", date = Sys.Date(), year = 0)
+#' fipe_vehicle(model = "etios platinum", make = "toyota", date = Sys.Date(), year = c(0, 2019, 2018))
 #'
 #' @export
 #'
